2c241e2f092db994d726e20dcecac87d90f31c3f
unknown <ebussman@gmail.com>
2023-06-06 18:32:28 -0700

    start of having data points appear on client display

--- src/App.js
+++ src/App.js
@@ -2,9 +2,23 @@ import React, { useState } from "react";
 import "./style.css";
 
 function App() {
-
-  const [tileData, setTileData] = useState([["one", "one and a half"],["two"]]);
-
+  const [tileData, setTileData] = useState([
+    [
+      { id: 1, name: "one" },
+      { id: 2, name: "two" },
+      { id: 3, name: "three" },
+    ],
+    [
+      { id: 4, name: "four" },
+      { id: 5, name: "five" },
+      { id: 6, name: "six" },
+    ],
+    [
+      { id: 7, name: "seven" },
+      { id: 8, name: "eight" },
+      { id: 9, name: "nine" },
+    ],
+  ]);
 
   return (
     <>
@@ -12,13 +26,19 @@ function App() {
         <h1>Wall Designer</h1>
         <div className="flex flex-row border border-black">
           <div className="border border-green-500">
-          <h1>side section</h1>
+            <h1>side section</h1>
           </div>
           <div className="flex flex-col border border-red-500">
             <h1>main body</h1>
-            <h1>
-              {tileData}
-              </h1>
+
+              {tileData.map((tileArrayData, index) => (
+                <ul key={index}>
+                  {tileArrayData.map(tile => (
+                    <l1 key={tile.id}>{tile.name}</l1>
+                  ))}
+                </ul>
+              ))}
+
           </div>
         </div>
       </div>
